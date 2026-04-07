c9a73dc89cf63a2b084c524b1562e5798c3cd0ed
Pranav samsung <pranavrao210@gmail.com>
2026-04-07 09:29:21 +0530

    creds added to env file

--- services/notificationScheduler.js
+++ services/notificationScheduler.js
@@ -2,10 +2,23 @@ const cron = require('node-cron');
 const admin = require('firebase-admin');
 const User = require('../models/user.model'); // Fix path
 
-// Init Firebase Admin
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: Buffer.from(process.env.FIREBASE_PRIVATE_KEY_BASE64, 'base64').toString(),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL
+};
+
 admin.initializeApp({
-  credential: admin.credential.cert(require('./serviceAccountKey.json'))
+  credential: admin.credential.cert(serviceAccount)
 });
+
 
 // Run every day at 8 PM
 cron.schedule('0 20 * * *', async () => {
